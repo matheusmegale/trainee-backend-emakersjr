561c3acd6eb9a16c4458edf75724328d2ab61c6f
Matheus Megale <matheusmegale5@gmail.com>
2024-12-12 11:20:58 -0300

    Primeiro Commit

---
+++ src/main/java/com/emakersjr/traineebackend/TraineebackendApplication.java
@@ -0,0 +1,13 @@
+package com.emakersjr.traineebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emakersjr/traineebackend/TraineebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emakersjr.traineebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
